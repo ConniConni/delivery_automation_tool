d2cb660de3d13c18398af5c8a6c3577a1ce8bedf
ConniConni <nmtgktmo@gmail.com>
2025-11-04 20:53:26 +0900

    load_config()を削除し、main()の戻り値にconfigを追加#3

--- main.py
+++ main.py
@@ -16,8 +16,8 @@
 
 import logging
 import sys
-import configparser
 import argparse
+import config_manager
 from pathlib import Path
 
 
@@ -25,37 +25,6 @@ from pathlib import Path
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
-
-
-def load_config(config_file_path: Path):
-    """
-    指定された設定ファイルを読み込み、ConfigParseオブジェクトを返す。
-    """
-
-    logging.info(f"設定ファイル {config_file_path} を読み込みます。")
-    config = configparser.ConfigParser()
-    try:
-        if not config_file_path or not config_file_path.is_file():
-            raise FileNotFoundError(f"設定ファイルが見つかりません: {config_file_path}")
-
-        with open(config_file_path, "r") as f:
-            content = f.read()
-            if not "[" in content or not "]" in content:
-                logging.warning(
-                    f"設定ファイル {config_file_path} はINIファイルの形式ではありません。"
-                )
-                return config  # 例外ではなく、空のconfigを返す
-
-        config.read(config_file_path)
-        logging.info(f"設定ファイルの読み込み完了。セクション: {config.sections()}")
-
-    except configparser.MissingSectionHeaderError as e:
-        logging.error(f"設定ファイルの読み込み中にエラーが発生しました: {e}")
-        raise
-    except Exception as e:
-        logging.error(f"設定ファイルの読み込み中に予期せぬエラーが発生しました: {e}")
-        raise
-    return config
 
 
 def main():
@@ -104,8 +73,9 @@ def main():
         logging.info("ツリー出力先: 標準出力")
 
     try:
-        config = load_config(args.config_file_path)
+        config = config_manager.load_config(args.config_file_path)
         logging.info(f"設定ファイル: {args.config_file_path} を読み込みました。")
+        return config
     except FileNotFoundError:
         raise
     except Exception:
