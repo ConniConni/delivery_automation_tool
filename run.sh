6f399dc387723f3c8ce1f7c0bfff1961e0e7072f
ConniConni <nmtgktmo@gmail.com>
2025-11-16 22:28:05 +0900

    設定ファイル微調整

--- run.sh
+++ run.sh
@@ -4,11 +4,12 @@ echo "#### コピー開始 ####"
 python main.py
 
 echo "#### config.ini修正 ####"
-sed -i '' 's|/Users/koni/Desktop/delivery_automation_tool/check/walk_base_sample/マイグレ|/Users/koni/Desktop/delivery_automation_tool/check/walk_base_sample/サンプル2|g' config.ini
+sed -i '' 's|/Users/koni/Desktop/walk_base_sample/マイグレ|/Users/koni/Desktop/walk_base_sample/サンプル2|g' config.ini
+cat ./config.ini
 echo "#### コピー開始 ####"
 python main.py
 
-sed -i '' 's|/Users/koni/Desktop/delivery_automation_tool/check/walk_base_sample/サンプル2|/Users/koni/Desktop/delivery_automation_tool/check/walk_base_sample/マイグレ|g' config.ini
+sed -i '' 's|/Users/koni/Desktop/walk_base_sample/サンプル2|/Users/koni/Desktop/walk_base_sample/マイグレ|g' config.ini
 
 echo "#### ツリー出力 ####"
 cd /Users/koni/Desktop/copy_teams
