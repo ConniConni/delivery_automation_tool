6d2bf839e0efab1579ab86a717382067aeb66a51
ConniConni <nmtgktmo@gmail.com>
2025-11-16 21:59:59 +0900

    run.shを修正　何度実行しても同じ処理になるように修正

--- run.sh
+++ run.sh
@@ -3,5 +3,13 @@
 echo "#### コピー開始 ####"
 python walk_mini_app.py
 
+echo "#### config.ini修正 ####"
+sed -i '' 's|/Users/koni/Desktop/base_teams/マイグレ|/Users/koni/Desktop/base_teams/サンプル2|g' config.ini
+echo "#### コピー開始 ####"
+python walk_mini_app.py
+
+sed -i '' 's|/Users/koni/Desktop/base_teams/サンプル2|/Users/koni/Desktop/base_teams/マイグレ|g' config.ini
+
 echo "#### ツリー出力 ####"
-tree ./walk_copy_place/マイグレ > tree.txt
+cd /Users/koni/Desktop/copy_teams
+tree > tree.txt
